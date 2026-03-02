a2fec1084508ba1dd54d85e8a2f4b255fb052a4e
ssmith86 <samuel.jp.smith@gmail.com>
2026-03-01 22:08:21 -0800

    added user icon

--- react-app/src/components/NavigationBar.jsx
+++ react-app/src/components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import eventy_logo from '../assets/eventy_logo.png';
 import './NavigationBar.css';
+import UserProfileIconButton from "./UserProfileIconButton";
 
 export default function NavigationBar() {
     return(
@@ -8,6 +9,8 @@ export default function NavigationBar() {
                 <div className="navbar-logo">
                     <img src={eventy_logo} alt="Eventy Logo" />
                 </div>
+
+                <UserProfileIconButton />
             </div>
         </header>
     );
---
+++ react-app/src/components/UserProfileIconButton.jsx
@@ -0,0 +1,11 @@
+import { MdAccountCircle, MdKeyboardArrowDown } from "react-icons/md";
+import "./UserProfileIconButton.css";
+
+export default function UserProfileIconButton(){
+    return(
+        <button className="profile-menu" type="button">
+            <MdAccountCircle className="profile-icon" />
+            <MdKeyboardArrowDown className="chevron-icon" />
+        </button>
+    );
+}
