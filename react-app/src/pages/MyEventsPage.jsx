7304f3e58e7a51e259edb282caa4cb28b7c0d2aa
kangnade <kangnade@gmail.com>
2026-03-24 13:11:47 -0700

    Implemented imports and useState variables

--- react-app/src/pages/MyEventsPage.jsx
+++ react-app/src/pages/MyEventsPage.jsx
@@ -1,8 +1,17 @@
 import NavigationBar from "../components/NavigationBar";
 import SearchBar from "../components/SearchBar";
+import { SearchBar } from "../components/SearchBar";
 import SideBar from "../components/SideBar";
+import { useState, useEffect } from "react";
+import EventCard from "../components/EventCard";
 
 export default function MyEventsPage() {
+  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [myEvents, setMyEvents] = useState([]);
+  const [savedEvents, setSavedEvents] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [hasSearched, setHasSearched] = useState(false);
+
   return (
     <div className="page-wrapper">
       <NavigationBar.jsx />
@@ -39,7 +48,7 @@ export default function MyEventsPage() {
             <h2 className="section-title">My Events</h2>
             <p className="section-subtitle">Events you have created</p>
             <div className="events-scroll-container">
-              {/* Display and render upcoming event via a function renderEventsGrid */}
+              {/* Display and render my event via a function renderEventsGrid */}
             </div>
           </section>
 
@@ -48,7 +57,7 @@ export default function MyEventsPage() {
             <h2 className="section-title">My Saved Events</h2>
             <p className="section-subtitle">Events you have saved</p>
             <div className="events-scroll-container">
-              {/* Display and render upcoming event via a function renderEventsGrid */}
+              {/* Display and render my saved event via a function renderEventsGrid */}
             </div>
           </section>
         </div>
