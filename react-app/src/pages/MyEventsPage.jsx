b41d4f80efd3228abd021a93c9057840888d7a30
kangnade <kangnade@gmail.com>
2026-03-24 12:50:53 -0700

    Implemented a structural design for MyEventPage component.

--- react-app/src/pages/MyEventsPage.jsx
+++ react-app/src/pages/MyEventsPage.jsx
@@ -1,5 +1,38 @@
-export const MyEventsPage = () => {
-    return(
-        <h1>My Events</h1>
-    );
+import NavigationBar from "../components/NavigationBar";
+import SearchBar from "../components/SearchBar";
+import SideBar from "../components/SideBar";
+
+export default function MyEventsPage() {
+  return (
+    <div className="page-wrapper">
+      <NavigationBar.jsx />
+
+      <div className="page-body">
+        {/* Display SideBar here */}
+        <aside className="sidebar-area">
+          <SideBar />
+        </aside>
+      </div>
+
+      <div className="main-content">
+        <div className="search-bar-area">
+          <SearchBar
+            setSearchResults={setSearchResults}
+            setHasSearched={setHasSearched}
+          />
+
+          {/* Display search result if the user has searched events */}
+
+          {/* Display the section - Upcoming Events */}
+          <section className="events-section"></section>
+
+          {/* Display the section - My Events */}
+          <section className="events-section"></section>
+
+          {/*Display the section - My Saved Events */}
+          <section className="events-section"></section>
+        </div>
+      </div>
+    </div>
+  );
 }
