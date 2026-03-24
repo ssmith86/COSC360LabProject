5d2a0093796762e969154872e276f9230439550d
ssmith86 <samuel.jp.smith@gmail.com>
2026-03-23 23:35:24 -0700

    created pages and routing

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,57 +2,15 @@ import "./App.css";
 import NavigationBar from "./components/NavigationBar";
 import { SearchBar } from "./components/SearchBar";
 import EventCard from "./components/EventCard";
-import sportImg from "./assets/sportImage.webp";
 import SignUpForm from "./components/SignUpForm";
 import LoginForm from "./components/LoginForm";
-import { useState } from "react";
 import EventCreationForm from "./components/EventCreationForm";
+import {Routes } from './Routes.jsx';
 
 function App() {
-  const [searchResults, setSearchResults] = useState([]);
-  const [hasSearched, setHasSearched] = useState(false);
-
   return (
     <div className="App">
-      <NavigationBar />
-      <SearchBar
-        setSearchResults={setSearchResults}
-        setHasSearched={setHasSearched}
-      />
-      {hasSearched ? (
-        searchResults.length === 0 ? (
-          <p className="no-results">No results found.</p>
-        ) : (
-          searchResults.map((item, index) => (
-            <EventCard
-              key={index}
-              image={sportImg}
-              title={item.event.name}
-              startDateTime={item.event.start_date}
-              endDateTime={item.event.end_date}
-              location={`${item.event.location.street}, ${item.event.location.city}`}
-              isSaved={0}
-              isOwner={0}
-              isAdmin={0}
-            />
-          ))
-        )
-      ) : (
-        <EventCard
-          image={sportImg}
-          title="Alamo Bowl 2025"
-          startDateTime="12/31/2025 17:00"
-          endDateTime="12/31/2025 23:00"
-          location="Alamodome, San Antonio, TX"
-          isSaved={1}
-          isOwner={1}
-          isAdmin={1}
-        />
-      )}
-
-      <EventCreationForm />
-      <LoginForm />
-      <SignUpForm />
+      <Routes/>
     </div>
   );
 }
---
+++ react-app/src/Routes.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Routes as RouterRoutes, Route } from 'react-router-dom';
+import { AdministrationDashboard } from './pages/AdministrationDashboard';
+import { EventDetailsPage } from './pages/EventDetailsPage';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LogInPage';
+import { MyEventsPage } from './pages/MyEventsPage';
+import { NewEventCreationPage } from './pages/NewEventCreationPage';
+import { RegistrationPage } from './pages/RegisterationPage';
+import { RegisteredUserDashboard } from './pages/RegisteredUserDashboard';
+import { UnregisteredUserPage } from './pages/UnregisteredUserPage';
+
+export const Routes = () => {
+    return(
+        <Router>
+            <RouterRoutes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/admin" element={<AdministrationDashboard />} />
+                <Route path="/event/:eventId" element={<EventDetailsPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/my-events" element={<MyEventsPage />} />
+                <Route path="/new-event" element={<NewEventCreationPage />} />
+                <Route path="/register" element={<RegistrationPage />} />
+                <Route path="/dashboard" element={<RegisteredUserDashboard />} />
+                <Route path="/browse" element={<UnregisteredUserPage />} />
+            </RouterRoutes>
+        </Router>
+    )
+}
