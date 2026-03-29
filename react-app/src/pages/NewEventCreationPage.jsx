f61bdbf7db4310f3d7b94fdff35b6410ae1a640a
kangnade <kangnade@gmail.com>
2026-03-29 00:42:33 -0700

    Added EventCreationForm component to NewEventCreationPage

--- react-app/src/pages/NewEventCreationPage.jsx
+++ react-app/src/pages/NewEventCreationPage.jsx
@@ -1,13 +1,15 @@
-import { NavigationBar } from '../components/NavigationBar';
-import { SideBar } from '../components/SideBar';
+import { NavigationBar } from "../components/NavigationBar";
+import { SideBar } from "../components/SideBar";
 import { SearchBar } from "../components/SearchBar";
+import EventCreationForm from "../components/EventCreationForm";
 
 export const NewEventCreationPage = () => {
-    return(
-        <>
-            <NavigationBar/>
-            <SearchBar/>
-            <SideBar/>
-        </>
-    );
-}
+  return (
+    <>
+      <NavigationBar />
+      <SearchBar />
+      <SideBar />
+      <EventCreationForm />
+    </>
+  );
+};
