996d37fa8d6c465f17534066a2e5500e70667254
ssmith86 <samuel.jp.smith@gmail.com>
2026-03-31 18:06:52 -0700

    set up with working demo test

---
+++ react-app/Tests/login.test.js
@@ -0,0 +1,9 @@
+
+// delete this this was just to test setup and doesn't test anything in the code
+import { describe, test, expect } from 'vitest';
+
+describe('Login Tests', () => {
+  test('should pass a basic test', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
