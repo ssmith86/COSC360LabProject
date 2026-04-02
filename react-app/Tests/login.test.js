b0849edecb782e80ea4186947da6b8b85dec8f17
ssmith86 <samuel.jp.smith@gmail.com>
2026-04-01 19:51:15 -0700

    test running instructions

--- react-app/Tests/login.test.js
+++ react-app/Tests/login.test.js
@@ -1,4 +1,6 @@
 
+//to run tests, cd into react-app and run `npm test` in the terminal
+
 // delete this this was just to test setup and doesn't test anything in the code
 import { describe, test, expect } from 'vitest';
 
