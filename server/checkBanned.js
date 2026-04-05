e8dfe1e51355199d8537988c9d0ed400ac1a64ff
ErenSun408 <syx20040408@163.com>
2026-04-04 22:07:27 -0700

    update checkBanned.js to use mongoose

--- server/checkBanned.js
+++ server/checkBanned.js
@@ -1,16 +1,12 @@
-const {getDB} = require("./db");
-const { ObjectId } = require("mongodb");
+const mongoose = require("mongoose");
+const User = require("./models/User");
 
 const checkBanned = async (req, res, next) => {
     const userId = req.body?.userId || req.query?.userId;
-    if (!userId || !ObjectId.isValid(userId)) return next();
+    if (!userId || !mongoose.Types.ObjectId.isValid(userId)) return next();
 
     try {
-        const db = getDB();
-        const user = await db.collection("users").findOne(
-            { _id: new ObjectId(userId) },
-            { projection: { isBanned: 1 } }
-        );
+        const user = await User.findById(userId).select("isBanned");
         if (user?.isBanned) {
             return res.status(403).json({ message: "Your account has been banned" });
         }
