19211fc9bfa57e6c30fa7fbd1a27be65368d8220
kangnade <kangnade@gmail.com>
2026-04-10 01:27:32 -0700

    Updated vite.config.js with a proxy for local dev purpose

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    include: ['Tests/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+  server: {
+    proxy: {
+      "/api": "http://localhost:3001",
+      "/search": "http://localhost:3001",
+      "/uploads": "http://localhost:3001",
+    },
   },
-})
+  test: {
+    environment: "jsdom",
+    include: ["Tests/**/*.{test,spec}.{js,ts,jsx,tsx}"],
+  },
+});
